b5ad4e5a8845ec25bb38eff5fe36803f9bab3691
logan <logancroberts@outlook.com>
2023-08-29 13:50:06 -0400

    Add spring initializr package files

---
+++ src/main/java/UNCG/CSC340Fall2023/ClassAssignment/ClassAssignmentApplication.java
@@ -0,0 +1,13 @@
+package UNCG.CSC340Fall2023.ClassAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UNCG/CSC340Fall2023/ClassAssignment/ClassAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package UNCG.CSC340Fall2023.ClassAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
