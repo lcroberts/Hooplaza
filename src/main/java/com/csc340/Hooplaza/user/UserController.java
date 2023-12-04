8c83957e6665a9838bd6ba26aa20935829baa9c3
Logan Roberts <logancroberts@outlook.com>
2023-12-04 18:20:14 -0500

    Add community list to user pages

--- src/main/java/com/csc340/Hooplaza/user/UserController.java
+++ src/main/java/com/csc340/Hooplaza/user/UserController.java
@@ -1,5 +1,6 @@
 package com.csc340.Hooplaza.user;
 
+import com.csc340.Hooplaza.community.Community;
 import com.csc340.Hooplaza.post.Post;
 import com.csc340.Hooplaza.post.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,15 +29,36 @@ public class UserController {
     @GetMapping({"", "/", "/bookmarks"})
     public String menu(Model model) {
         User user = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        List<Community> communityList = user.getCommunities();
+        for (int i = 0; i < communityList.size(); ) {
+            Community community = communityList.get(i);
+            if (!community.isCommunityActive()) {
+                communityList.remove(community);
+            } else {
+                i++;
+            }
+        }
         List<Post> posts = user.getBookmarks();
         model.addAttribute("postList", posts);
+        model.addAttribute("communityList", communityList);
         return "user/bookmarks";
     }
 
     @GetMapping({"/board"})
     public String board(Model model) {
+        User user = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
         List<Post> posts = postService.getAllPosts();
+        List<Community> communityList = user.getCommunities();
+        for (int i = 0; i < communityList.size(); ) {
+            Community community = communityList.get(i);
+            if (!community.isCommunityActive()) {
+                communityList.remove(community);
+            } else {
+                i++;
+            }
+        }
         model.addAttribute("postList", posts);
+        model.addAttribute("communityList", communityList);
         return "user/board";
     }
 
